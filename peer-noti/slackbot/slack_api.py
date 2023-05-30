0124e4da38f72d9b38b069d3836a0dd02c284ba2
jeongmin <yesuel1111@naver.com>
2023-05-30 22:11:31 +0900

    [feat] Create demo web page for send DM
    - send_dm.html: 메시지 데모 페이지 템플릿 구현
    - index.html: 메인 페이지 구현
    - form error, alert_message에 대한 페이지 템플릿 구현
    - slack_api.py: post_message() 함수에서 인자로 받은 message를 전송
    - views.py: 구현한 템플릿 페이지로 렌더링 및 성공과 실패에 대한 message
      추가
    - forms.py: 페이지에서 입력받은 정보를 저장하는 UserForm 구현
    
    Resolves: #9

--- peer-noti/slackbot/slack_api.py
+++ peer-noti/slackbot/slack_api.py
@@ -19,7 +19,7 @@ class SlackAPI:
                 return member['id']
         raise Http404("id: [" + intra_id + "]에 해당하는 유저가 존재하지 않습니다.")
 
-    def post_message(self, channel_id, name):
+    def post_message(self, channel_id, name, message):
         """
         슬랙 채널에 메세지 보내기
         channel: user id를 넣을 경우 user에게, channel id를 넣을 경우 channel에게 메세지가 전송됨
@@ -27,7 +27,7 @@ class SlackAPI:
         try:
             result = self.client.chat_postMessage(
                 channel=channel_id,
-                text="Hello " + name
+                text= name + "님 안녕하세요! \n" + message
             )
         except SlackApiError as e:
             print(f"Error posting message: {e}")
--- peer-noti/slackbot/views.py
+++ peer-noti/slackbot/views.py
@@ -1,29 +1,28 @@
-from django.http import HttpResponse
+from django.contrib import messages
 from django.http import Http404
 from .slack_api import SlackAPI
+from django.shortcuts import render, redirect
+from .forms import UserForm
+
 
 def index(request):
-    return HttpResponse("peer-noti slackbot 페이지입니다.<br>"
-                        "dm을 보내려면 [http://127.0.0.1:8000/slackbot/send/dm/]로 이동하세요!")
-
+    return render(request, 'slackbot/index.html')
 
 def send_dm(request):
-    try:
-        intra_id = request.GET.get('id')
-        if intra_id is None:
-            raise Http404("id: Param not found. <br>"
-                          "dm을 보낼 유저의 intra id를 아래 주소의 파라미터로 넣어주세요! <br>"
-                          "http://127.0.0.1:8000/slackbot/send/dm/?id=[intra_id]")
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            try:
+                intra_id = form.cleaned_data['intra_id']
+                message = form.cleaned_data['message']
+                slack = SlackAPI()
+                user_id = slack.get_user_id(intra_id)
+                slack.post_message(user_id, intra_id, message)
+                messages.success(request, "메시지가 성공적으로 전송되었습니다.")
+                return redirect('slackbot:send_dm')
 
-        slack = SlackAPI()
-        user_id = slack.get_user_id(intra_id)
-        slack.post_message(user_id, intra_id)
-
-
-    except Http404 as e:
-        # 요청 매개변수가 없는 경우 예외 처리
-        response = HttpResponse(str(e))
-        response.status_code = 404
-        return response
-
-    return HttpResponse(intra_id + "님께 DM을 전송합니다.")
+            except Http404 as e:
+                messages.error(request, str(e))
+    else:
+        form = UserForm()
+    return render(request, 'slackbot/dm_form.html', {'form': form})
